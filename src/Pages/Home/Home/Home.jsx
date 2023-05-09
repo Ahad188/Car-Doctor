6dbee89a4647ea24481586ee4db00b0aa18474a0
AhaD <abuahad35188@gmail.com>
2023-05-09 23:53:35 +0600

    carusol added

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,8 +1,13 @@
+import Banner from "../Banner/Banner";
+
+ 
+
  
 
 const Home = () => {
      return (
           <div>
+              <Banner></Banner>
                <h1>This is home</h1>
           </div>
      );
