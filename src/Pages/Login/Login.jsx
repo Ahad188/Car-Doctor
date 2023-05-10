c1ad4085647ca805a19bc51468b36b86be461b67
AhaD <abuahad35188@gmail.com>
2023-05-10 14:39:05 +0600

    singup page set up

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -34,7 +34,7 @@ const Login = () => {
                                 <input className="btn btn-primary" type="submit" value="Login" />
                             </div>
                         </form>
-                        <p className='my-4 text-center'>New to Car Doctors <Link className='text-orange-600 font-bold' to="/signup">Sign Up</Link> </p>
+                        <p className='my-4 text-center'>New to Car Doctors <Link className='text-orange-600 font-bold' to="/singup">Sign Up</Link> </p>
                     </div>
                 </div>
             </div>
--- src/Routers/Routers.jsx
+++ src/Routers/Routers.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home/Home";
 import Login from "../Pages/Login/Login";
+import Singup from "../Pages/Singup/Singup";
 
 const router = createBrowserRouter([
      {
@@ -14,6 +15,10 @@ const router = createBrowserRouter([
           },{
                path:'/login',
                element:<Login></Login>
+          },
+          {
+               path:'singup',
+               element:<Singup></Singup>
           }
        ]
      },
