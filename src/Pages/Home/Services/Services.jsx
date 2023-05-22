191bdfdad3586d60d0ef3fb3effff4ea58a74674
AhaD <abuahad35188@gmail.com>
2023-05-22 20:47:45 +0600

    service page a add to search option and sort

--- src/Pages/Home/Services/Services.jsx
+++ src/Pages/Home/Services/Services.jsx
@@ -1,14 +1,22 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ServicesCard from "./ServicesCard";
 
 const Services = () => {
   const [services, setServices] = useState([]);
-  console.log(services);
+  const [asc, setAsc] = useState(true)
+  const searchRef = useRef(null);
+  const [search, setSearch] = useState('');
+//   console.log(services);
   useEffect(() => {
-    fetch("https://car-doctor-server-beige.vercel.app/services")
+     // https://car-doctor-server-beige.vercel.app
+    fetch(`http://localhost:5000/services?search=${search}&sort=${asc ? 'asc' : 'Desc'}`)
       .then((res) => res.json())
       .then((data) => setServices(data));
-  }, []);
+  }, [asc,search]);
+  const handleSearch = () => {
+     console.log(searchRef.current.value);
+     setSearch(searchRef.current.value);
+ }
   return (
     <div className="mt-4">
       <div className="text-center">
@@ -16,8 +24,23 @@ const Services = () => {
         <h2 className="text-5xl">Our Service Area</h2>
         <p>
           the majority have suffered alteration in some form, by injected humour, or randomised{" "}
-          <br /> words which do not look even slightly believable.{" "}
+          <br /> words which do not look even slightly believable.   
         </p>
+        <div className="form-control">
+                    <div className="input-group">
+                        <input type="text" ref={searchRef} placeholder="Search…" className="input input-bordered" />
+                        <button onClick={handleSearch} className="btn btn-square">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                        </button>
+                    </div>
+                </div>
+        <button
+                className="btn btn-primary"
+                onClick={()=>setAsc(!asc)}
+               
+               >
+                    {asc ? "Price: high to Low" : "Price: Low to high"}
+               </button>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {services.map((service) => (
